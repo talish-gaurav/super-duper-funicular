ea0a2bbf243be0b4561664436920edc745342499
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-10-23 19:14:08 +0100

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,9 @@
+print("This application olnly marks out of 100")
+math=int(input("Enter Math marks"))
+english=int(input("Enter English marks"))
+Science=int(input("Enter Science marks"))
+French=int(input("Enter French marks"))
+
+sum= math+english+Science+French
+percentage =sum/400*100
+print("Your percentage is:", percentage )
